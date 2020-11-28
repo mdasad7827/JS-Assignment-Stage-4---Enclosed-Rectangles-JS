50b4d109bf079ce59d187c499a7a046c0fd3c18e
mdasad7827 <mohaammadasad1996@gmail.com>
2020-11-28 16:19:36 +0530

    children

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@
 //	left: '96px',
 //	width: '64px',
 //	height: '96px',
-//      Children: []
+//      children: []
 //}
 
 const { Children } = require("react");
@@ -11,10 +11,10 @@ const { Children } = require("react");
 function updateStructure(rec1, rec2) {
   if (contains(rec1, rec2)) {
     const relativeDim = relative(rec1, rec2);
-    return { ...rec1, Children: [relativeDim] };
+    return { ...rec1, children: [relativeDim] };
   } else if (contains(rec2, rec1)) {
     const relativeDim = relative(rec2, rec1);
-    return { ...rec2, Children: [relativeDim] };
+    return { ...rec2, children: [relativeDim] };
   } else {
     return { ...rec1 };
   }
@@ -25,7 +25,7 @@ function relative(rec1, rec2) {
   const rec2n = normalise(rec2);
 
   const res = {
-    Children: rec2.Children,
+    children: rec2.children,
   };
 
   if (rec2.top) {
